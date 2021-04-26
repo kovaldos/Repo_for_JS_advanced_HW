0e7ed97df99e4bda13ccb0b3068f4fda44528fa4
Yaroslav Kovalev <yakovaldos@yandex.ru>
2021-04-26 17:51:33 +0300

    HW6 Update
    Anotrher changes in SearchComponent

--- HW6/project/js/SearchComponent.js
+++ HW6/project/js/SearchComponent.js
@@ -1,10 +1,8 @@
 Vue.component("search-form", {
-    props: ["value"],
     template:`
         <form action="#" class="header__form" @submit.prevent="$parent.filter">
         <input type="text" class="header__search"
-        v-bind:value="value"
-        v-on:input="$emit('input', $event.target.value)">
+        v-model="$parent.searchLine">
         <button type="submit" class="search-btn"><i class="fas fa-search"></i></button>
         </form>`
 });
